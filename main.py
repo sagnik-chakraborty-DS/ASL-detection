9eb91ffb063d98a6c4f54d60336406a1873ddbc2
sagnik-chakraborty-DS <sagnikchakss@gmail.com>
2023-04-07 19:06:45 +0530

    crop hand

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 import cv2
 import time
+import math
+import numpy as np
 from cvzone.HandTrackingModule import HandDetector
 ################################
-wCam, hCam = 2000, 2000
+# wCam, hCam = 2000, 2000
 ################################
 
 cap = cv2.VideoCapture(1)
-cap.set(3, wCam)
-cap.set(4, hCam)
+# cap.set(3, wCam)
+# cap.set(4, hCam)
 pTime = 0
 
-detector = HandDetector(maxHands=1)
-
+detector = HandDetector(maxHands=1, detectionCon=.9)
+imgSize = 300
 while True:
     success, img = cap.read()
-    detector.findHands(img)
+    hands, img = detector.findHands(img)
+    if hands:
+        hand = hands[0]
+        x,y,w,h = hand["bbox"]
+
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8)*255
+        imgCrop = img[y-20:y+h+20, x-20:x+w+20]
+
+
+
+        aspectRatio = h/w
+        if aspectRatio>1:
+            k = imgSize/h
+            wCal = math.ceil(k*w)
+            imgResize = cv2.resize(imgCrop, (wCal,imgSize))
+            imgResizeShape = imgResize.shape
+            imgWhite[0:imgResizeShape[0], 0:imgResizeShape[1]] = imgCrop
+
+        cv2.imshow("ImgCrop", imgCrop)
+        cv2.imshow("ImgWhite", imgWhite)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
@@ -23,4 +44,5 @@ while True:
                 1, (255, 0, 0), 3)
 
     cv2.imshow("Img", img)
+
     cv2.waitKey(1)
